52cb7fbac0ad0874d9b08f3bd5b91179f5d5719f
Zetajota <joelduraznoflores@gmail.com>
2026-03-10 23:37:25 -0400

    Agregando clases del proyecto State

--- src/strategy/estrategiasConcretas/EnvioTerrestre.ts
+++ src/strategy/estrategiasConcretas/EnvioTerrestre.ts
@@ -3,6 +3,7 @@ import { EstrategiaEnvio } from '../EstrategiaEnvio';
 export class EnvioTerrestre implements EstrategiaEnvio {
   calcularCosto(): number {
     console.log("Calculando costo de envío terrestre...");
+ 
     return 15.0; // Costo fijo de ejemplo
   }
 }
